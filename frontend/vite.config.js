6836ede33c75d39fbeab285f9d0851c9fc891261
Rishikanth R <rishikanth2121@gmail.com>
2025-06-29 12:14:16 +0530

    Add vercel.json to fix routing 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -12,3 +13,7 @@ export default defineConfig({
   },
   base: './', // ✅ makes sure relative paths work
 })
+
+
+
+
